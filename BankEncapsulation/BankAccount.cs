4cf86f0ac4ea98a70f3b61212a258287fec9a017
TJ Vinsavich <tjvinsavich@gmail.com>
2021-06-21 11:37:18 -0500

    initial commit

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankEncapsulation
+{
+    public class BankAccount
+    {
+        public BankAccount()
+        {
+        }
+
+        private double balance = 0;
+
+        public void Deposit(double value)
+        {
+            balance = value;
+        }
+
+        public double GetBalance()
+        {
+            return balance;
+        }
+    }
+}
---
+++ BankEncapsulation/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace BankEncapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount checking = new BankAccount();
+
+            Console.WriteLine("Welcome to your bank account!");
+            Console.WriteLine("How much money will you deposit?");
+
+            double input = double.Parse(Console.ReadLine());
+
+            checking.Deposit(input);
+
+            Console.WriteLine($"Your new balance is {checking.GetBalance()}");
+
+
+        }
+    }
+}
